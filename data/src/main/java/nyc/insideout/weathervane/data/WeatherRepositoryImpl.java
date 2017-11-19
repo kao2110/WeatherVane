2744a2e1abb54ee9e40bd1eb297f77e18104a51b
KevieKev <finetic.speller@gmail.com>
2017-11-18 19:11:03 -0500

    implement WeatherRepository.

--- data/src/main/java/nyc/insideout/weathervane/data/WeatherRepositoryImpl.java
+++ data/src/main/java/nyc/insideout/weathervane/data/WeatherRepositoryImpl.java
@@ -1,38 +1,65 @@
 package nyc.insideout.weathervane.data;
 
-import nyc.insideout.weathervane.data.preferences.WeatherPreferences;
+import android.support.v4.util.ArrayMap;
+
+import nyc.insideout.weathervane.data.mapper.WeatherDataMapper;
 import nyc.insideout.weathervane.data.service.WeatherService;
+
+import nyc.insideout.weathervane.data.database.model.WeatherData;
+import nyc.insideout.weathervane.data.preferences.WeatherPreferences;
+import nyc.insideout.weathervane.data.service.model.ApiWeatherData;
 import nyc.insideout.weathervane.domain.interactor.GetForecastDetailsUseCase;
-import nyc.insideout.weathervane.domain.interactor.GetForecastUseCase;
+import nyc.insideout.weathervane.domain.interactor.GetForecastUseCase.RequestResult;
+import nyc.insideout.weathervane.domain.model.ForecastDetail;
 import nyc.insideout.weathervane.domain.repository.WeatherRepository;
 
 
 public class WeatherRepositoryImpl implements WeatherRepository {
 
-    WeatherService mWeatherService;
-    WeatherPreferences mWeatherPreferences;
+    private WeatherService mWeatherService;
+    private WeatherPreferences mWeatherPreferences;
+    private WeatherDataMapper mWeatherDataMapper;
+    private ArrayMap<Long, WeatherData> mWeatherDataCache;
 
     public WeatherRepositoryImpl(WeatherService weatherService,
-                                 WeatherPreferences weatherPreferences){
-        this.mWeatherService = weatherService;
-        this.mWeatherPreferences = weatherPreferences;
+                                 WeatherPreferences weatherPreferences,
+                                 WeatherDataMapper weatherDataMapper){
+        mWeatherService = weatherService;
+        mWeatherPreferences = weatherPreferences;
+        mWeatherDataMapper = weatherDataMapper;
     }
 
     @Override
-    public void getForecast(String location, DataRequestCallback<GetForecastUseCase.RequestResult> callback){
+    public void getForecast(final String location, final DataRequestCallback<RequestResult> callback){
 
+        if(mWeatherDataCache != null){
+            RequestResult requestResult =
+                    new RequestResult(location, mWeatherDataMapper.cacheToDomain(mWeatherDataCache.values()));
+            callback.onDataLoaded(requestResult);
+        }else{
+            mWeatherService.fetchForecast(location, new WeatherService.WeatherServiceCallback<ApiWeatherData>() {
+                @Override
+                public void onDataLoaded(ApiWeatherData result) {
+                    mWeatherDataCache = mWeatherDataMapper.apiToCache(result);
+                    RequestResult requestResult =
+                            new RequestResult(location, mWeatherDataMapper.cacheToDomain(mWeatherDataCache.values()));
+                    callback.onDataLoaded(requestResult);
+                }
+
+                @Override
+                public void onError(Throwable e) {
+
+                }
+            });
+        }
     }
 
     @Override
     public void getForecastDetails(long date, DataRequestCallback<GetForecastDetailsUseCase.RequestResult> callback){
-
-    }
-
-    private void setRecentSearchLocation(String location){
-
-    }
-
-    private String getRecentSearchLocation(){
-        return null;
+        if(mWeatherDataCache != null){
+            ForecastDetail forecastDetail = mWeatherDataMapper.cacheToDomainDetail(mWeatherDataCache.get(date));
+            GetForecastDetailsUseCase.RequestResult result = new GetForecastDetailsUseCase.RequestResult(forecastDetail);
+            callback.onDataLoaded(result);
+        }
     }
 }
---
+++ data/src/test/java/nyc/insideout/weathervane/data/WeatherRepositoryImplTest.java
@@ -0,0 +1,100 @@
+package nyc.insideout.weathervane.data;
+
+import android.support.v4.util.ArrayMap;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import nyc.insideout.weathervane.data.database.model.WeatherData;
+import nyc.insideout.weathervane.data.mapper.WeatherDataMapper;
+import nyc.insideout.weathervane.data.preferences.WeatherPreferences;
+import nyc.insideout.weathervane.data.service.WeatherService;
+import nyc.insideout.weathervane.data.service.model.ApiWeatherData;
+import nyc.insideout.weathervane.domain.interactor.GetForecastDetailsUseCase;
+import nyc.insideout.weathervane.domain.interactor.GetForecastUseCase;
+import nyc.insideout.weathervane.domain.repository.WeatherRepository;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class WeatherRepositoryImplTest {
+
+    @Captor
+    private ArgumentCaptor<WeatherService.WeatherServiceCallback<ApiWeatherData>> weatherServiceCallbackCaptor;
+
+    @Mock
+    private WeatherRepository.DataRequestCallback<GetForecastUseCase.RequestResult> forecastListDataRequestCallback;
+
+    @Mock
+    private WeatherRepository.DataRequestCallback<GetForecastDetailsUseCase.RequestResult> forecastDetailsDataRequestCallback;
+
+    @Mock
+    private WeatherService weatherService;
+
+    @Mock
+    private ArrayMap<Long, WeatherData> cache;
+
+    @Mock
+    private ApiWeatherData apiWeatherData;
+
+    @Mock
+    private WeatherPreferences weatherPreferences;
+
+    @Mock
+    private WeatherDataMapper weatherDataMapper;
+
+    private WeatherRepository weatherRepository;
+    private String location = "Ytown";
+
+
+    @Before
+    public void setup(){
+        MockitoAnnotations.initMocks(this);
+        apiWeatherData = TestData.getApiWeatherData();
+        doReturn(cache).when(weatherDataMapper).apiToCache(apiWeatherData);
+        weatherRepository = new WeatherRepositoryImpl(weatherService, weatherPreferences, weatherDataMapper);
+    }
+
+    @Test
+    public void whenCacheIsEmpty_ThenRequestForecastListFromService(){
+        // When forecast data requested from repository first time
+        weatherRepository.getForecast(location, forecastListDataRequestCallback);
+
+        // Then forecast data requested from repository
+        verify(weatherService).fetchForecast(eq(location), any(WeatherService.WeatherServiceCallback.class));
+    }
+
+    @Test
+    public void whenCacheHasData_ThenRequestForecastListFromCache(){
+        // When forecast data requested from repository first time
+        weatherRepository.getForecast(location, forecastListDataRequestCallback);
+        verify(weatherService).fetchForecast(eq(location), weatherServiceCallbackCaptor.capture());
+        weatherServiceCallbackCaptor.getValue().onDataLoaded(apiWeatherData);
+
+        // Then on second request data requested from cache
+        weatherRepository.getForecast(location, forecastListDataRequestCallback);
+        verify(weatherService).fetchForecast(eq(location), any(WeatherService.WeatherServiceCallback.class));
+        verify(cache, times(2)).values();
+    }
+
+    @Test
+    public void whenCacheHasData_ThenRequestForecastDetailsFromCache(){
+        // Given forecast data has already been requested
+        weatherRepository.getForecast(location, forecastListDataRequestCallback);
+        verify(weatherService).fetchForecast(eq(location), weatherServiceCallbackCaptor.capture());
+        weatherServiceCallbackCaptor.getValue().onDataLoaded(apiWeatherData);
+
+        // When forecast details are requested
+        weatherRepository.getForecastDetails(100L, forecastDetailsDataRequestCallback);
+
+        // Then data obtained from cache
+        verify(cache).get(100L);
+    }
+}
